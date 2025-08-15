f29562f61611bf89e10cdf43434e8557382e925a
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-14 23:42:22 -0500

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+const MONGO_URI = 'your_mongo_db_connection_string';
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
