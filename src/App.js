9523a62e6c047e110a5158ff310e3b8f8e4c7818
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-14 23:42:21 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Interactive Wireframe Generator</h1>
+            <p>Drag and drop your components here!</p>
+        </div>
+    );
+};
+
+export default App;
