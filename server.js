09d6d4359cae48a75230944d9e9fce2afe6238b6
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-08-14 23:47:36 -0500

    Add error handling middleware for better error management

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
